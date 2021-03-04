8fc3c93611da54ce31dde1f9f5e5cfda5ccec200
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-03-04 22:29:42 +0100

    feat: solve typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = 0;
+    if (typeof array !== 'undefined' && array.length) {
+      minValue = array.sort(function(a, b) {return a - b}).shift();
+      return minValue;
+    } else return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = 0;
+  if (typeof array !== 'undefined' && array.length) {
+    maxValue = array.sort(function(a, b) {return b - a}).shift();
+    return maxValue;
+  } else return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgValue = 0;
+  if (typeof array !== 'undefined' && array.length) {
+    avgValue = array.reduce((function(total, num) {return total + num})) / array.length;
+    return avgValue;
+  } else return avgValue;
 }
